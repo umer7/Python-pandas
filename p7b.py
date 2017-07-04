eaeda95bca1e7767a7d6b91f0c8c76b7c4f21b4d
Umer Farooq <k142229@nu.edu.pk>
2017-07-04 17:09:24 +0500

    Create p7b.py

---
+++ p7b.py
@@ -0,0 +1,41 @@
+import quandl
+import pandas as pd
+import pickle
+
+# Not necessary, I just do this so I do not show my API key.
+api_key = open('quandlapikey.txt','r').read()
+
+def state_list():
+    fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+    return fiddy_states[0][0][1:]
+    
+
+def grab_initial_state_data():
+    states = state_list()
+
+    main_df = pd.DataFrame()
+
+    for abbv in states:
+        query = "FMAC/HPI_"+str(abbv)
+        df = quandl.get(query, authtoken=api_key)
+        print(query)
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = pd.concat([main_df,df],axis=1)
+
+            
+    pickle_out = open('fiddy_states.pickle','wb')
+    pickle.dump(main_df, pickle_out)
+    pickle_out.close()        
+
+    
+#grab_initial_state_data()
+
+pickle_in = open('fiddy_states.pickle','rb')
+HPI_data = pickle.load(pickle_in)
+print(HPI_data)
+
+HPI_data.to_pickle('pickle.pickle')
+HPI_data2 = pd.read_pickle('pickle.pickle')
+print(HPI_data2)
